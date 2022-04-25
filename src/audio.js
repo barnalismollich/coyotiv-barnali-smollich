2fb6853f693415de7d5c4cc83a295c5b3eac235a
Barnali Smollich <barnaliingermany@gmail.com>
2022-04-25 16:50:58 +0200

    Deleted studynotes

---
+++ src/audio.js
@@ -0,0 +1,8 @@
+class Audio {
+  constructor(audioname) {
+    this.audioname = audioname
+    // this.uploadedOn = uploadDate;
+  }
+}
+
+module.exports = Audio
---
+++ src/document.js
@@ -0,0 +1,8 @@
+class Document {
+  constructor(documentName) {
+    this.documentName = documentName
+    // this.uploadedOn = uploadDate;
+  }
+}
+
+module.exports = Document
