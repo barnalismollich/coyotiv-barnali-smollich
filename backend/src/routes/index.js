7724dcd209c12bb1ee5010a928cc46ca33a4ab69
Barnali Smollich <barnaliingermany@gmail.com>
2022-06-06 15:46:43 +0200

    Solve errors of authentication

--- backend/src/routes/index.js
+++ backend/src/routes/index.js
@@ -2,7 +2,7 @@ const express = require('express')
 const router = express.Router()
 /* GET home page. */
 router.get('/', (req, res) => {
-  res.render('index', { title: 'Family Diary' })
+  res.render('index', { title: 'blablalblal' })
 })
 router.get('/bootstrap', (req, res) => {
   res.render('bootstrap', { title: 'bootstrap' })
