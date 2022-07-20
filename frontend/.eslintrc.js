9c8771f8e48c53184707b6f95b351e07d042124b
Barnali Smollich <barnaliingermany@gmail.com>
2022-07-20 15:47:49 +0200

    Include comments and correct file to finalize project

--- frontend/.eslintrc.js
+++ frontend/.eslintrc.js
@@ -7,6 +7,7 @@ module.exports = {
   // parserOptions: {
   //   parser: '@babel/eslint-parser',
   // },
+  // babel causes too many errors therefore it is deactivated
   rules: {
     'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
