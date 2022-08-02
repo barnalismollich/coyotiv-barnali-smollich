36ab8589522441d8a9b050dcaf609d760aebfb83
Barnali Smollich <barnaliingermany@gmail.com>
2022-08-02 21:03:03 +0200

    Include comments in files to increase usability

--- backend/jest.config.js
+++ backend/jest.config.js
@@ -1,3 +1,4 @@
 module.exports = {
   setupFilesAfterEnv: ['./jest.setup.js'],
 }
+// The jest files are important for running the tests.
--- backend/jest.setup.js
+++ backend/jest.setup.js
@@ -1,1 +1,2 @@
 jest.setTimeout(30000)
+// The jest files are important for running the tests.
